2bacdfbe0592181733460e798ac22a350542ddbd
NaishaSabnani <83396298+NaishaSabnani@users.noreply.github.com>
2021-07-07 21:02:53 +0530

    Update block.js

--- block.js
+++ block.js
@@ -1,6 +1,9 @@
 class Block{
     constructor(x, y, width, height) {
         var options = {
+          restitution:0.4,
+          
+          friction:0.4
                      
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
